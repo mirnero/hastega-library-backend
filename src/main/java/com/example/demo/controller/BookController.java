002b70d8d61c17d8b7f1d34ec28b1e800d2aa2cd
mirko <m.busonero@iperurania.it>
2023-05-19 15:54:57 +0200

    CORS ERROR fix

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -18,6 +18,7 @@ import org.springframework.data.domain.Page;
 import jakarta.persistence.criteria.Predicate;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/books")
+@CrossOrigin(origins = "*")
 public class BookController {
 
     private final BookRepository bookRepository;
